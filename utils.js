60ca029b64ab7ce38c13ca0a75c7f36936cd1bb4
Josh Hunt <josh@trtr.co>
2014-06-15 18:00:39 +1000

    Remove debug middleware

--- utils.js
+++ utils.js
@@ -5,9 +5,9 @@ module.exports.error = function (res, message, statusCode) {
 };
 
 module.exports.catchJsonErrors = function (err, req, res, next){
+    console.error(err.stack);
     if (err instanceof SyntaxError) {
         module.exports.error(res, 'Could not decode request: JSON parsing failed', 400);
-    } else {
-        module.exports.error(res, 'Unexpected server error occured');
     }
+    next(err);
 };
