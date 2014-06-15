a8b7d5cc4c7999a7a4706dd2c65ebfb72cf72226
Josh Hunt <josh@trtr.co>
2014-06-15 18:06:10 +1000

    Catch more JSON errors
    And lets keep the debugging middleware in for now...

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ module.exports.error = function (res, message, statusCode) {
 
 module.exports.catchJsonErrors = function (err, req, res, next){
     console.error(err.stack);
-    if (err instanceof SyntaxError) {
+    if (err instanceof SyntaxError || err.message.indexOf('json') > -1) {
         module.exports.error(res, 'Could not decode request: JSON parsing failed', 400);
     }
     next(err);
