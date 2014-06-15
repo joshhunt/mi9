2c9189fffc2a9af51ebe0f51d4b12ea1cc01388e
Josh Hunt <josh@trtr.co>
2014-06-15 17:38:04 +1000

    Another oops, was still referencing old error func

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-module.exports.catchJsonErrors = function (err, req, res, next){
-    if (err instanceof SyntaxError) {
-        errorResponse(res, 'Could not decode request: JSON parsing failed', 400);
-    } else {
-        errorResponse(res, 'Unexpected server error occured');
-    }
-};
-
 module.exports.error = function (res, message, statusCode) {
     statusCode = statusCode || 500;
     message = message || 'Unexplained error';
     res.status(statusCode).json({error: message});
+};
+
+module.exports.catchJsonErrors = function (err, req, res, next){
+    if (err instanceof SyntaxError) {
+        module.exports.error(res, 'Could not decode request: JSON parsing failed', 400);
+    } else {
+        module.exports.error(res, 'Unexpected server error occured');
+    }
 };
