946295bd49574feba4a6237c615a5c2fcab49bb5
Josh Hunt <josh@trtr.co>
2014-06-15 17:50:50 +1000

    Added some debugging middleware

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var controllers = require('./controllers'),
     utils = require('./utils.js');
 
 var app = express();
+
+app.use(function (req, res, next){
+    console.log('oh fuck it');
+    console.log(req.method, req.originalUrl);
+    req.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+    });
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(utils.catchJsonErrors);
 
