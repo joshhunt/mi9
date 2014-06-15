24ceae106765d223dbe6d56bea528a316b94744b
Josh Hunt <josh@trtr.co>
2014-06-15 17:30:20 +1000

    Fixed derp for OR operator

--- utils.js
+++ utils.js
@@ -2,12 +2,12 @@ module.exports.catchJsonErrors = function (err, req, res, next){
     if (err instanceof SyntaxError) {
         errorResponse(res, 'Could not decode request: JSON parsing failed', 400);
     } else {
-        next(err);
+        errorResponse(res, 'Unexpected server error occured');
     }
 };
 
 module.exports.error = function (res, message, statusCode) {
     statusCode = statusCode || 500;
-    message = message | 'Unexplained error';
+    message = message || 'Unexplained error';
     res.status(statusCode).json({error: message});
 };
